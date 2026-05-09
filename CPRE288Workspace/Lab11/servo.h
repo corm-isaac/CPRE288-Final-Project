116bb3f66013e781de6c6d2c5d0893693df095cb
corm-isaac <cormier.isaac@gmail.com>
2026-05-08 20:59:24 -0500

    Update servo.h

--- CPRE288Workspace/Lab11/servo.h
+++ CPRE288Workspace/Lab11/servo.h
@@ -1,20 +1,20 @@
 /*
- * servo.h
+ * @file servo.h
+ * @brief
+ * Intialization and movement and calibration of the servo on the cybot
+ * Update: Added scan functions for PING and IR
+ * @author Cooper Sanders, Isaac Cormier
+ * @date 04/07/26
  *
- *  Created on: Apr 7, 2026
- *      Author: cooperrs
  */
-
 #ifndef SERVO_H_
 #define SERVO_H_
-
 
 #include <stdint.h>
 #include <stdbool.h>
 #include <inc/tm4c123gh6pm.h>
 #include "lcd.h"
 #include "button.h"
-
 
 void servo_init(void);
 
@@ -23,8 +23,8 @@ void servo_move(float degrees);
 // init lcd, button, and timer before calling
 void servo_calibrate(void);
 
-uint16_t IRScan(float degrees);
+uint16_t IR_scan(float degrees);
 
-float PingScan(int degree);
+float ping_scan(int degree);
 
 #endif /* SERVO_H_ */
