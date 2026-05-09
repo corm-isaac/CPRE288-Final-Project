055b59a7967da0c0806f16f5d93ee186e2d34553
corm-isaac <cormier.isaac@gmail.com>
2026-05-08 21:00:58 -0500

    Update map.h

--- CPRE288Workspace/Lab11/map.h
+++ CPRE288Workspace/Lab11/map.h
@@ -1,19 +1,22 @@
 /*
- * map.h
- *
- *  Created on: May 3, 2026
- *      Author: ikcorm
+ * @file map.h
+ * @brief
+ * if you haven't read path.c yet you probably should but this is supposed to go with path.c in the sense the autonomous bot
+ * is supposed to determine the object coordinates. the map was supposed to have a 2D map with coordinates of every object
+ * but i didn't have enough time so unfortunately the only glimpse into my mind is this method. this is also untested for same reasons
+ * @author Isaac Cormier
+ * @date 5/3/2026
  */
 
 #ifndef MAP_H_
 #define MAP_H_
 
-
-typedef struct{
+typedef struct
+{
     float x;
     float y;
-}Point;
+} Point;
 
-Point determineObjectCoordinates(int objectId);
+Point determine_object_coordinates(int object_id);
 
 #endif /* MAP_H_ */
