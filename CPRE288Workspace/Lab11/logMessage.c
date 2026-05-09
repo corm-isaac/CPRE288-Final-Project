d7267f4aa5d8f1026f08b4f2a4314d3264341d31
corm-isaac <cormier.isaac@gmail.com>
2026-05-08 21:01:22 -0500

    Update logMessage.c

--- CPRE288Workspace/Lab11/logMessage.c
+++ CPRE288Workspace/Lab11/logMessage.c
@@ -1,8 +1,8 @@
 /*
- * logMessage.c
- *
- *  Created on: Apr 10, 2026
- *      Author: ikcorm
+ * @file logMessage.c
+ * @brief print method to uartStr
+ * @date 04/10/26
+ * @author Isaac Cormier
  */
 
 #include "Timer.h"
@@ -11,8 +11,14 @@
 #include <stdbool.h>
 #include "driverlib/interrupt.h"
 #include "manual.h"
-
-void logMessage(size_t bufSize, const char *fmt, ...)
+/*
+ * @brief prints whatever we want to uartStr (designed to bypass the fact that uartStr can only take 1 arg, which makes it annoying
+ * to print values to uart without having another buffer and sprintf) definetly did all of this by myself with no outside help
+ * @author Isaac Cormier
+ * @param str buffer size, then message of what you want to print, formatted like normal!
+ * @date 04/14/26
+ */
+void log_message(size_t buf_Size, const char *fmt, ...)
 {
 
     char buffer[bufSize];
@@ -20,22 +26,14 @@ void logMessage(size_t bufSize, const char *fmt, ...)
     va_list args;
     va_start(args, fmt);
 
-    vsnprintf(buffer, bufSize, fmt, args);
+    vsnprintf(buffer, buf_Size, fmt, args);
 
     va_end(args);
 
     // Output to LCD
-    //lcd_clear();
-    //lcd_printf("%s", buffer);
+    // lcd_clear();
+    // lcd_printf("%s", buffer);
 
     // Output to UART
     uart_sendStr(buffer);
 }
-
-void logMessage_init(){
-    timer_init(); // Must be called before lcd_init(), which uses timer functions
-    lcd_init();
-}
-
-
-
