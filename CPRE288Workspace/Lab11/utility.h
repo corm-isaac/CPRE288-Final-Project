56641673478970e76900fdb61194188fa2c7dd59
corm-isaac <cormier.isaac@gmail.com>
2026-05-08 20:58:47 -0500

    Update utility.h

--- CPRE288Workspace/Lab11/utility.h
+++ CPRE288Workspace/Lab11/utility.h
@@ -1,8 +1,11 @@
 /*
- * utility.h
+ * @file utility.h
+ * @brief random code that is too cool to fit elsewhere. In this instance it's mostly the object detections
+ * logic.
  *
- *  Created on: Mar 24, 2026
- *      Author: isaac
+ *  @author Cooper Sanders, Isaac Cormier
+ *
+ *  @date 03/24/2026
  */
 
 #ifndef UTILITY_H_
@@ -11,49 +14,46 @@
 #include <math.h>
 
 // ---- Constants ----
-#define COUNT               90      // Number of scan points
-#define RAW_IR_THRESHOLD    10      // IR spike/smoothing threshold
-#define EDGE_SCALE          1.5f    // Edge detection scaling factor
+#define COUNT 90            // Number of scan points
+#define RAW_IR_THRESHOLD 10 // IR spike/smoothing threshold
+#define EDGE_SCALE 1.5f     // Edge detection scaling factor
 
 // ---- Structs ----
 
-typedef struct {
-    int   angle;   // Angle in degrees
-    int   IR;      // IR sensor reading (cm)
-    float ping;    // Ping sensor reading (cm)
-} ScanPoint;
+typedef struct
+{
+    int angle;  // Angle in degrees
+    int IR;     // IR sensor reading (cm)
+    float ping; // Ping sensor reading (cm)
+} scan_point;
 
-typedef struct {
-    int   primary_id;      // Object ID
-    int   start_angle;     // Start angle (degrees)
-    int   end_angle;       // End angle (degrees)
-    int   middle_angle;    // Middle angle (degrees)
-    float distance_cm;     // Distance from ping sensor (cm)
-    int   radial_width;    // Angular width (degrees)
+typedef struct
+{
+    int primary_id;    // Object ID
+    int start_angle;   // Start angle (degrees)
+    int end_angle;     // End angle (degrees)
+    int middle_angle;  // Middle angle (degrees)
+    float distance_cm; // Distance from ping sensor (cm)
+    int radial_width;  // Angular width (degrees)
 } Obstacle;
 
 // ---- Global Variables ----
 
-extern char       STOP_BYTE;
-extern ScanPoint  scanPointArray[90];
-extern Obstacle   objectArray[30];
+extern char STOP_BYTE;
+extern ScanPoint g_scan_point_array[90];
+extern Obstacle g_object_array[30];
 
 // ---- Function Prototypes ----
 
 // Lab 11 - Object Detection
-void initalizeObject(int objectId, int startAngle, int endAngle, float distance_cm);
-int  objectDetermination(void);
-void printObjects(int num);
-void cleanGlobals();
+void initalize_object(int object_id, int start_angle, int end_angle, float distance_cm);
+int object_determination(void);
+void print_objects(int num);
+void clean_globals();
 
 // Data Cleaning
 void clean_data(int data[], int output[]);
 
 // Edge Detection
-int  edge_detection(Obstacle obstacles[], int data[], float ping_data[]);
-int  find_smallest_object(Obstacle obstacles[], int numObstacles, float ping_data[]);
-void print_obstacles(Obstacle obstacles[], int numObstacles, float ping_data[]);
-
-
 void suck(int time);
 #endif /* UTILITY_H_ */
