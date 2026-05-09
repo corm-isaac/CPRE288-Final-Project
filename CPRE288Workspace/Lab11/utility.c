af8503a217ed280677a10b0895f82152a22d8b6f
corm-isaac <cormier.isaac@gmail.com>
2026-05-08 20:58:30 -0500

    Update utility.c

--- CPRE288Workspace/Lab11/utility.c
+++ CPRE288Workspace/Lab11/utility.c
@@ -1,20 +1,14 @@
 /*
- * utility.c
+ * @file utility.c
+ * @brief random code that is too cool to fit elsewhere. In this instance it's mostly the object detections
+ * logic. and random code
  *
- *  Created on: Mar 24, 2026
- *      Author: cooperrs
+ *  @author Cooper Sanders, Isaac Cormier
+ *
+ *  @date 03/24/2026
  */
 
 #include "utility.h"
-
-/*
- * utility.c
- *
- *  Created on: Mar 24, 2026
- *      Author: cooperrs
- */
-
-
 #include "Timer.h"
 #include "lcd.h"
 
@@ -29,234 +23,197 @@
 #include "logMessage.h"
 #include "boundary.h"
 #include "manual.h"
-//GLOBAL VARIABLES - Very pretty unsafe but i dont care
-char STOP_BYTE = 0; //Designed to be manual override to stop the bot. not yet implemented 5/2
-ScanPoint scanPointArray[90]; //An array of ScanPoint objects - ScanPoint objects store the angle, IR and PING value at each angle increment
-Obstacle objectArray[30]; //An array of objects that we have
+// GLOBAL VARIABLES - Very pretty unsafe but i dont care
+char STOP_BYTE = 0;                // Designed to be manual override to stop the bot. not yet implemented 5/2
+Scan_point g_scan_point_array[90]; // An array of ScanPoint objects - ScanPoint objects store the angle, IR and PING value at each angle increment
+Obstacle g_object_array[30];       // An array of objects that we have
 
-void logScan()
+/*
+ *  @brief prints out scan from scanPointArray
+ * @author Isaac Cormier, Cooper Sanders
+ * @param (takes global scanPointArray)
+ * @date 04/26/26
+ */
+void log_scan()
 {
-    logMessage(20, "SCAN_START:\r\n");
+    // logMessage(20, "SCAN_START:\r\n");
     int i;
-    for(i = 0; i < 90; i++){
-        //logMessage(150, "Angle: %d IR_Value: %d Ping: %.2f\r\n", scanPointArray[i].angle , scanPointArray[i].IR, scanPointArray[i].ping);
+    for (i = 0; i < 90; i++)
+    {
+        // logMessage(150, "Angle: %d IR_Value: %d Ping: %.2f\r\n", scanPointArray[i].angle , scanPointArray[i].IR, scanPointArray[i].ping);
         logMessage(150, "SCAN:%d %d %.2f\r\n",
-                   scanPointArray[i].angle,
-                   scanPointArray[i].IR,
-                   scanPointArray[i].ping
-        );
+                   g_scan_point_array[i].angle,
+                   g_scan_point_array[i].IR,
+                   g_scan_point_array[i].ping);
     }
-    logMessage(20, "SCAN_END:\r\n");
+    // logMessage(20, "SCAN_END:\r\n");
 }
 
 // #### OBJECT DETECTION LAB 11 - ISAAC's CODE PROBABLY WILL EXPLODE ####
+/*
+ *  @brief init object
+ * @author Mila Haynes, Isaac Cormier,
+ * @param (takes global objectArray)
+ * @date 04/26/26
+ */
 
-void initalizeObject(int objectId, int startAngle, int endAngle, float distance_cm){
-    objectArray[objectId].primary_id = objectId;
-    objectArray[objectId].start_angle = startAngle;
-    objectArray[objectId].end_angle = endAngle;
-    objectArray[objectId].middle_angle = (startAngle + endAngle) / 2;
-    objectArray[objectId].distance_cm = distance_cm;
-    objectArray[objectId].radial_width = (endAngle - startAngle); //rad
+void initalize_object(int object_id, int start_angle, int end_angle, float distance_cm)
+{
+    object_array[object_id].primary_id = object_id;
+    object_array[object_id].start_angle = start_angle;
+    object_array[object_id].end_angle = end_angle;
+    object_array[object_id].middle_angle = (start_angle + end_angle) / 2;
+    object_array[object_id].distance_cm = distance_cm;
+    object_array[object_id].radial_width = (end_angle - start_angle); // rad
 }
 
-//MAKE SURE SCANPOINTARR CONTAINS DATA BEFORE CALLING THIS FUNCTION
-int objectDetermination(/*Modifies objectArray; reads from scanPointArray*/){ //Populates global objectArray with objects from the scanPoint Array Data
-    int objectCount = 0; //object counter
-    int j; //outer loop
-    int IR_effectiveRange = 45; //45 CM
+/*
+ *  @brief determines objects from scanPointArray. most of the code here was taken from our lab7 object determination which was mostly
+ * written  by Mila, modified merely to use both IR and Ping seamlessly.
+ * @author Mila Haynes, Isaac Cormier
+ * @param (takes global objectArray)
+ * @date 04/26/26
+ */
+
+// MAKE SURE SCANPOINTARR CONTAINS DATA BEFORE CALLING THIS FUNCTION
+int object_determination(/*Modifies objectArray; reads from scanPointArray*/)
+{                                // Populates global objectArray with objects from the scanPoint Array Data
+    int object_count = 0;        // object counter
+    int j;                       // outer loop
+    int IR_effective_range = 45; // 45 CM
     int IR_tolerance = 5;
-    //int ping_effectiveRange = 100; //100 CM
-    for (j = 0; j < 90; j++) {
-        float ping_distance = scanPointArray[j].ping; //reads current ping distance of one point from scanPointArray
-        int IR_distance = scanPointArray[j].IR;//reads current IR distance of one point from scanPointArray
-        int angle = scanPointArray[j].angle; //reads current angle value of one point from scanPoint Array; could be achieved by reading j too
+    // int ping_effectiveRange = 100; //100 CM
+    for (j = 0; j < 90; j++)
+    {
+        float ping_distance = g_scan_point_array[j].ping; // reads current ping distance of one point from scanPointArray
+        int IR_distance = g_scan_point_array[j].IR;       // reads current IR distance of one point from scanPointArray
+        int angle = g_scan_point_array[j].angle;          // reads current angle value of one point from scanPoint Array; could be achieved by reading j too
 
-        //PRIMARILY WILL READ FROM IR for determining object width; and use PING for distance herself
+        // PRIMARILY WILL READ FROM IR for determining object width; and use PING for distance herself
 
-        if (IR_distance < IR_effectiveRange){
-            //Need to find out if we have an object by checking the next next value (basically i+2); Assuming that all objects are at least 6 degrees long (ie 3 similar values in a row)
-            if (j > 87) continue; // avoid j+2 overflow
+        if (IR_distance < IR_effective_range)
+        {
+            // Need to find out if we have an object by checking the next next value (basically i+2); Assuming that all objects are at least 6 degrees long (ie 3 similar values in a row)
+            if (j > 87)
+                continue; // avoid j+2 overflow
 
-            int ir2 = scanPointArray[j+2].IR;
+            int ir2 = g_scan_point_array[j + 2].IR;
 
-            if (ir2 < IR_distance - IR_tolerance || ir2 > IR_distance + IR_tolerance) continue; //tests false case
+            if (ir2 < IR_distance - IR_tolerance || ir2 > IR_distance + IR_tolerance)
+                continue; // tests false case
 
-            int beginningAngle = angle;
-            float distance = ping_distance; //yoink from PING because its more accuarte
+            int beginning_angle = angle;
+            float distance = ping_distance; // yoink from PING because its more accuarte
 
-            int IR_base_distance = scanPointArray[j].IR; //compares a base distance which moves for every next value, improving object detection (found in prev labs)
+            int IR_base_distance = g_scan_point_array[j].IR; // compares a base distance which moves for every next value, improving object detection (found in prev labs)
 
-            while (j < 90) { //i  like windows
-                int next_IR = scanPointArray[j].IR;
-                if (abs(next_IR - IR_base_distance) > IR_tolerance) break;
+            while (j < 90)
+            { // i  like windows
+                int next_IR = g_scan_point_array[j].IR;
+                if (abs(next_IR - IR_base_distance) > IR_tolerance)
+                    break;
                 IR_base_distance = next_IR; // slide the window forward
                 j++;
             }
 
-            int endingAngle = scanPointArray[j-1].angle;
+            int ending_angle = g_scan_point_array[j - 1].angle;
 
-            int primitive_radial_width = endingAngle - beginningAngle;
-            if(primitive_radial_width < 5){
+            int primitive_radial_width = ending_angle - beginning_angle;
+            if (primitive_radial_width < 5)
+            {
                 continue;
             }
 
-            //Initialize Object
-            initalizeObject(objectCount, beginningAngle, endingAngle, distance);
-            objectCount++;
-
+            // Initialize Object
+            initalize_object(object_count, beginning_angle, ending_angle, distance);
+            object_count++;
         }
     }
-    return objectCount;
+    return object_count;
 }
 
-void printObjects(int num){ //number of objects to iterate thru; thinking this param should take input from objectDet
-    //logMessage(100, "Object # |  Angle |  Distance(PING) | Radial Width");
+/*
+ *  @brief prints the object outs
+ * @author Mila Haynes, Isaac Cormier, Cooper Sanders
+ * @param (takes global scanPointArray)
+ * @date 04/26/26
+ */
+
+void print_objects(int num)
+{ // number of objects to iterate thru; thinking this param should take input from objectDet
+    // logMessage(100, "Object # |  Angle |  Distance(PING) | Radial Width");
 
     int i;
-    for (i = 0; i < num; i++) {
-        //logMessage(100, "Object: %d | Start Angle: %d; End Angle: %d; Middle Angle: %d |  Distance: %.2f | Width: %d\r\n", objectArray[i].primary_id, objectArray[i].start_angle, objectArray[i].end_angle, objectArray[i].middle_angle, objectArray[i].distance_cm, objectArray[i].radial_width);
+    for (i = 0; i < num; i++)
+    {
+        // logMessage(100, "Object: %d | Start Angle: %d; End Angle: %d; Middle Angle: %d |  Distance: %.2f | Width: %d\r\n", objectArray[i].primary_id, objectArray[i].start_angle, objectArray[i].end_angle, objectArray[i].middle_angle, objectArray[i].distance_cm, objectArray[i].radial_width);
         logMessage(100, "OBJECT:%d %d %d %d %.2f %d\r\n",
-                   objectArray[i].primary_id,
-                   objectArray[i].start_angle,
-                   objectArray[i].end_angle,
-                   objectArray[i].middle_angle,
-                   objectArray[i].distance_cm,
-                   objectArray[i].radial_width
-        );
-
+                   g_object_array[i].primary_id,
+                   g_object_array[i].start_angle,
+                   g_object_array[i].end_angle,
+                   g_object_array[i].middle_angle,
+                   g_object_array[i].distance_cm,
+                   g_object_array[i].radial_width);
     }
 }
 
-
 // #### END OF LAB 11 ####
-
+/*
+ *  @brief mr. wishy washy
+ * @author Cooper Sanders
+ * @param none
+ * @date 04/26/26
+ */
 
 void clean_data(int data[], int output[])
 {
     int temp[COUNT];
     int i = 0;
 
-    for (i = 0; i < COUNT; i++) {
+    for (i = 0; i < COUNT; i++)
+    {
         temp[i] = data[i];
     }
 
     // remove spikes
     for (i = 1; i < COUNT - 1; i++)
     {
-        int diff_prev = abs(temp[i] - temp[i-1]);
-        int diff_next = abs(temp[i] - temp[i+1]);
+        int diff_prev = abs(temp[i] - temp[i - 1]);
+        int diff_next = abs(temp[i] - temp[i + 1]);
 
         if (diff_prev > RAW_IR_THRESHOLD && diff_next > RAW_IR_THRESHOLD)
         {
-            temp[i] = (temp[i-1] + temp[i+1]) / 2;
+            temp[i] = (temp[i - 1] + temp[i + 1]) / 2;
         }
     }
 
     // fix last element spike
-    if (abs(temp[COUNT-1] - temp[COUNT-2]) > RAW_IR_THRESHOLD)
+    if (abs(temp[COUNT - 1] - temp[COUNT - 2]) > RAW_IR_THRESHOLD)
     {
-        temp[COUNT-1] = temp[COUNT-2];
+        temp[COUNT - 1] = temp[COUNT - 2];
     }
 
     // edge preserving smoothing
-    for (i = 0; i < COUNT; i++) {
+    for (i = 0; i < COUNT; i++)
+    {
         float sum = temp[i];
         int count = 1;
 
-
         // left neighbor
-        if (i > 0 && abs(temp[i] - temp[i-1]) < RAW_IR_THRESHOLD) {
-            sum += temp[i-1];
+        if (i > 0 && abs(temp[i] - temp[i - 1]) < RAW_IR_THRESHOLD)
+        {
+            sum += temp[i - 1];
             count++;
         }
 
         // right neighbor
-        if (i < COUNT - 1 && abs(temp[i] - temp[i+1]) < RAW_IR_THRESHOLD) {
-            sum += temp[i+1];
+        if (i < COUNT - 1 && abs(temp[i] - temp[i + 1]) < RAW_IR_THRESHOLD)
+        {
+            sum += temp[i + 1];
             count++;
         }
 
         output[i] = (int)(sum / count);
-    }
-}
-
-//Fills obstacles[] with obstacle data
-int edge_detection(Obstacle obstacles[], int data[], float ping_data[])
-{
-    int count_up = 0;
-    int count_down = 0;
-    int ups[10];
-    int downs[10];
-    int i = 0;
-
-    int current_search_direction = 1; //1==up, 0==down
-
-    int last_distance = data[0];
-    for (i = 3; i < COUNT; i++)
-    {
-        if (current_search_direction && data[i] > last_distance * EDGE_SCALE)
-        {
-            ups[count_up] = i;
-            count_up++;
-            current_search_direction = !current_search_direction;
-        }
-
-        else if (!current_search_direction && data[i]< last_distance * (2 - EDGE_SCALE))
-        {
-            downs[count_down] = i;
-            count_down++;
-            current_search_direction = !current_search_direction;
-        }
-
-        last_distance = data[i-2]; //lag behind by 3
-    }
-
-    for (i = 0; i < count_down; i++)
-    {
-        obstacles[i].start_angle = ups[i] * 2; //stores actual angle
-        obstacles[i].end_angle = downs[i] * 2; //stores actual angle
-        obstacles[i].distance_cm = ping_data[(ups[i] + downs[i]) / (2)];
-    }
-
-    return count_down; //number of objects found
-}
-
-int find_smallest_object(Obstacle obstacles[], int numObstacles, float ping_data[])
-{
-    int angle = (obstacles[0].start_angle + obstacles[0].end_angle) / 2;
-    int smallest_linear_width = 2 * ping_data[angle/2] * sin((obstacles[0].end_angle - obstacles[0].start_angle) * 3.141592 / (2.0 * 180));
-    int smallest_linear_index = 0;
-    int i = 0;
-    for(i = 1; i < numObstacles; i++)
-    {
-        int angle = (obstacles[i].start_angle + obstacles[i].end_angle) / 2;
-        int linear_width = 2 * ping_data[angle/2] * sin((obstacles[i].end_angle - obstacles[i].start_angle) * 3.141592 / (2.0 * 180));
-        if(linear_width < smallest_linear_width)
-        {
-            smallest_linear_width = linear_width;
-            smallest_linear_index = i;
-        }
-
-    }
-    return smallest_linear_index;
-}
-
-void print_obstacles(Obstacle obstacles[], int numObstacles, float ping_data[]) {
-
-    char buffer[32];
-    sprintf(buffer, "Objects#  Angle  Distance  Width");
-    uart_sendStr(buffer);
-    //uart_sendNewLine();
-    int i = 0;
-    for(i = 0; i < numObstacles; i++)
-    {
-        int angle = (obstacles[i].start_angle + obstacles[i].end_angle) / 2;
-        //Find linear width via trig (Sin of dTheta / 2) = (half of linear width / distance). Solve for linear width. [Draw diagram]
-        int linear_width = 2 * ping_data[angle/2] * sin((obstacles[i].end_angle - obstacles[i].start_angle) * 3.141592 / (2.0 * 180)); //convert sin part to radians, also divide by 2
-        sprintf(buffer, "%-10d%-7d%-10.2f%-5d", i, angle, ping_data[angle/2], linear_width);
-        uart_sendStr(buffer);
-        //uart_sendNewLine();
     }
 }
 
@@ -268,4 +225,3 @@ void suck(int time)
     oi_uartSendChar(138);
     oi_uartSendChar(0);
 }
-
